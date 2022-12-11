7d4adad9e4a0b0db3c3023abc3758acf73498aa9
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-11 13:17:32 -0300

    initiating api request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,10 @@
-const request = require('request')
+const request = require('request');
 
 const apiKey = '524be49095a00669fc39e4eb593c2750';
-const apiURL = 'http://api.weatherstack.com/current?access_key=${apiKey}';
+const apiURL = `http://api.weatherstack.com/current?access_key=${apiKey}&query=37.8267,-122.4233&units=m`;
 
-request(apiURL)
-    .then((response) => console.log(response));
+request(apiURL, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
+ 
