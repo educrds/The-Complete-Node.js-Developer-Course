f95a914a7ec504f082f5a958d5e7ce3f7893a025
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-12 15:22:44 -0300

    add function geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,11 @@
 const request = require('request');
+const chalk = require('chalk')
 
 const apiKey = '524be49095a00669fc39e4eb593c2750';
 const apiURL = `http://api.weatherstack.com/current?access_key=${apiKey}&query=37.8267,-122.4233&units=m`;
 
-request(apiURL, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({url:apiURL, json:true}, (error, response) => {
+  const data = response.body.current;
+  console.log(chalk.green(`It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`));
 });
  
