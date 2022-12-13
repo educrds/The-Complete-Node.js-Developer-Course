b7beaf223f157d855014d7304698454bb67c982f
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-13 14:16:28 -0300

    add partials elements

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 const port = 5050;
 
 const app = express();
 
 // define paths for express config
 const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // setup static dir to serve
 app.use(express.static(publicDir));
 
+// express routes
 app.get('', (req, res) =>
   res.render('index', {
     title: 'Weather',
@@ -30,11 +36,29 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Need help?',
+    name: 'Eduardo',
     call: 'Call me: (321) 8909-09888',
   });
 });
 
 app.get('/weather', (req, res) => res.send('Weather today is...'));
+
+// 404 route
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title:'404',
+    error: 'Article not found',
+    name:'Eduardo'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title:'404',
+    error: 'Page not found',
+    name:'Eduardo'
+  });
+});
 
 // initialize server in a specific Port
 app.listen(port, () => console.log(`Server is running on port ${port}`));
