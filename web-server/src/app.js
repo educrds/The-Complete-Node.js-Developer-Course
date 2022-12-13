b086c0a20543b18069c556933612b4fd50b8f58e
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-13 13:13:07 -0300

    setup dynamic directory with hbs NPM package

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,36 @@ const path = require('path');
 const port = 5050;
 
 const app = express();
+
+// define paths for express config
 const publicDir = path.join(__dirname, '../public');
 
-app.use(express.static(publicDir))
+// setup handlebars engine and views location
+app.set('view engine', 'hbs');
+
+// setup static dir to serve
+app.use(express.static(publicDir));
+
+app.get('', (req, res) =>
+  res.render('index', {
+    title: 'Weather',
+    name: 'Eduardo',
+  })
+);
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Eduardo',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Need help?',
+    call: 'Call me: (321) 8909-09888',
+  });
+});
 
 app.get('/weather', (req, res) => res.send('Weather today is...'));
 
