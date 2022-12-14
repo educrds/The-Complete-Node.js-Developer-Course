c49db99843f23930aafc9cbd759f9066dff1023a
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-14 09:22:24 -0300

    finally setup front end search

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,25 @@
 console.log('Client side JS file is loaded');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+const locationText = document.getElementById('location-text');
+const forecastText = document.getElementById('forecast-text');
+
+weatherForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const location = search.value;
+  const urlAPI = `http://localhost:5050/weather?address=${location}`;
+
+  fetch(urlAPI)
+    .then((response) => response.json())
+    .then(({ location, forecast, error }) => {
+      if (error) locationText.textContent = error;
+      else {
+        locationText.innerHTML = location;
+        forecastText.innerHTML = forecast;
+      }
+    });
+}
