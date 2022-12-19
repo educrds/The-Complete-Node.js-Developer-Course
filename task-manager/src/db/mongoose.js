cfc83e3fa1bb649ceccc7fd5f2cd9e4ad0f35cf0
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-19 14:53:12 -0300

    creating REST Task API

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -3,16 +3,3 @@ const mongoose = require('mongoose');
 // Connecting DB
 mongoose.connect('mongodb://127.0.0.1:27017/db_task_manager');
 
-// Creating Task model
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,22 +1,36 @@
-const express = require('express');
 require('./db/mongoose');
+const express = require('express');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// passing data by POST, fetching the data and adding to the DB
+// passing User data by POST, fetching the data and adding to the DB
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user
     .save()
-    .then(() => res.send(user))
+    .then(() => res.status(201).send(user))
     .catch((e) => {
       // change response status
       res.status(400).send(e.message);
     });
 });
 
+// passing Task data by POST, fetching the data and adding to the DB
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => res.status(201).send(task))
+    .catch((e) => {
+      // change response status
+      res.status(400).send(e.message);
+    });
+});
+
+// listening server port
 app.listen(port, () => console.log(`Server is running on port ${port}`));
