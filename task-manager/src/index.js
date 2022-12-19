ee7afc8f4f5de4358b357af1f2658abcf07994f9
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-19 17:35:36 -0300

    change to async-await & setup PATCH route

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,44 +9,139 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Passing User data by POST, fetching the data and adding to the DB
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => res.status(201).send(user))
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
     // change response status
-    .catch((e) => res.status(400).send(e.message));
+    res.status(400).send(e.message);
+  }
 });
 
 // Reading Users
-app.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((e) => res.status(400).send(e.message));
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
 // Reading unique User by ID
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((e) => res.status(500).send());
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+// Updating Unique User by ID
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdateds = ['name', 'email', 'password', 'age'];
+
+  const isValidOperation = updates.every((update) =>
+    allowedUpdateds.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid field(s) to update!' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// Updating Unique Task by ID
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdateds = ['description', 'completed'];
+
+  const isValidOperation = updates.every((update) =>
+    allowedUpdateds.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid field(s) to update!' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
 // Passing Task data by POST, fetching the data and adding to the DB
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    // change response status if error
-    .catch((e) => res.status(400).send(e.message));
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    // change response status
+    res.status(400).send(e.message);
+  }
+});
+
+// Reading tasks
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// Reading unique task by ID
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // listening server port
