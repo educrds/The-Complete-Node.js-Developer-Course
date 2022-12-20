9eaf133963f24f0920cd77638b0f98293690583e
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-20 15:45:20 -0300

    setup JsonWebToken

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,11 +1,18 @@
 require('./db/mongoose');
-const bcrypt = require('bcryptjs');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  if (req.method === 'GET') {
+    res.send('GET requests are disabled');
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 app.use(userRouter);
@@ -14,14 +21,15 @@ app.use(taskRouter);
 // listening server port
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+const jwt = require('jsonwebtoken');
+
 // Using bcrypt algorithm library for hash password
-// const myFunc = async () => {
-//   const password = 'edu1234';
-//   const hashedPassword = await bcrypt.hash(password, 8);
+const myFunc = async () => {
+  const token = jwt.sign({ _id: 'ábc123' }, 'thisismy', { expiresIn: '7 days' });
 
-//   const isMatch = await bcrypt.compare('edu1234', hashedPassword);
+  console.log(token);
 
-//   console.log(isMatch);
-// };
+  console.log(jwt.verify(token, 'thisismy'));
+};
 
-// myFunc();
+myFunc();
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -8,7 +8,9 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    
+    res.status(201).send({ user, token });
   } catch (e) {
     // change response status
     res.status(400).send(e.message);
@@ -18,9 +20,10 @@ router.post('/users', async (req, res) => {
 // Setup login route
 router.post('/users/login', async (req, res) => {
   try {
-    // const {email, password} = req.body
     const user = await User.findByCredentials(req.body.email, req.body.password);
-    res.send(user);
+    const token = await user.generateAuthToken();
+
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
