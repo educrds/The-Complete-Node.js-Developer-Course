7f403232a80f82a29863d7adf2a902c1a6ffafba
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-01-04 10:27:59 -0300

    alterado idioma

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
 
     socket.join(user.room);
 
-    socket.emit('message', generateMessage('Admin', 'Welcome!'));
+    socket.emit('message', generateMessage('Admin', 'Bem vindo!'));
     socket.broadcast
       .to(user.room)
       .emit('message', generateMessage('Admin', `${user.username} has joined!`));
