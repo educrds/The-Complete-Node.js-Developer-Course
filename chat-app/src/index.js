8f96a217636f71ae31e1ac87eb48e705f4101b62
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-01-04 10:30:14 -0300

    Revert "alterado idioma"
    This reverts commit 7f403232a80f82a29863d7adf2a902c1a6ffafba.

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
 
     socket.join(user.room);
 
-    socket.emit('message', generateMessage('Admin', 'Bem vindo!'));
+    socket.emit('message', generateMessage('Admin', 'Welcome!'));
     socket.broadcast
       .to(user.room)
       .emit('message', generateMessage('Admin', `${user.username} has joined!`));
